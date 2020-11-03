2cf3bfb434f1ba0c7c765ab317ad7ae6643cf86b
Diox <gt@ks.diox.es>
2020-11-03 13:12:18 +0000

    Working version upload

---
+++ ansible-roles.sh
@@ -0,0 +1,4 @@
+ansible-galaxy collection install community.general
+ansible-galaxy collection install community.digitalocean
+ansible-galaxy collection install community.crypto
+ansible-galaxy install elastic.elasticsearch,7.9.2
