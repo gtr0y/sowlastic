62143d9f270363a6d31cac021c17bd9a3d33cc9a
Diox <gt@ks.diox.es>
2020-11-03 17:26:46 +0000

    Working on Dynamic SSH

--- 0-ansible-roles.sh
+++ 0-ansible-roles.sh
@@ -1,4 +1,8 @@
+apt-get install python3-pip
+pip3 install ansible
+ssh-keygen -b 2048 -t rsa
 ansible-galaxy collection install community.general
 ansible-galaxy collection install community.digitalocean
 ansible-galaxy collection install community.crypto
 ansible-galaxy install elastic.elasticsearch,7.9.2
+git clone https://github.com/gtr0y/sowlastic.git
