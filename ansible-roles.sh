b395593099cc9f1d95e4883ff2d17a8e7c68aa4b
Diox <gt@ks.diox.es>
2020-11-03 13:38:25 +0000

    Changed debug output

--- ansible-roles.sh
+++
@@ -1,4 +0,0 @@
-ansible-galaxy collection install community.general
-ansible-galaxy collection install community.digitalocean
-ansible-galaxy collection install community.crypto
-ansible-galaxy install elastic.elasticsearch,7.9.2
