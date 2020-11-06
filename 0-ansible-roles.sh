2b3adb2b035dffc2e081bb6f7ad77146a8b3b65f
Diox <gt@ks.diox.es>
2020-11-06 12:06:53 +0000

    Bugfixes and minor improvements

--- 0-ansible-roles.sh
+++ 0-ansible-roles.sh
@@ -8,3 +8,12 @@ ansible-galaxy collection install community.crypto
 ansible-galaxy install elastic.elasticsearch,7.9.2
 export ANSIBLE_HOST_KEY_CHECKING=False
 git clone https://github.com/gtr0y/sowlastic.git
+cd sowlastic
+mkdir ./group_vars
+read -p "Enter API key: " APIKEY
+echo "digio_api_key: $APIKEY" >> ./group_vars/all
+echo "clustername: sowel-prod" >> ./group_vars/all
+echo "slug: s-1vcpu-2gb" >> ./group_vars/all
+echo "master_heap: 1g" >> ./group_vars/all
+echo "data_heap: 1g" >> ./group_vars/all
+echo "coord_heap: 1g" >> ./group_vars/all
