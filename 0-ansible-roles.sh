32bb1a2897f119497bf2d7cb315e75f8160d574e
Diox <gt@ks.diox.es>
2020-11-06 12:20:16 +0000

    Added missing package

--- 0-ansible-roles.sh
+++ 0-ansible-roles.sh
@@ -1,6 +1,7 @@
 #git clone https://github.com/gtr0y/sowlastic.git
 apt-get update
 apt-get install python3-pip -y
+apt-get install python3-jmespath -y
 pip3 install ansible
 ssh-keygen -q -t rsa -b 2048 -N '' -f ~/.ssh/id_rsa <<<n 2>&1 >/dev/null
 ansible-galaxy collection install community.general
@@ -8,7 +9,6 @@ ansible-galaxy collection install community.digitalocean
 ansible-galaxy collection install community.crypto
 ansible-galaxy install elastic.elasticsearch,7.9.2
 export ANSIBLE_HOST_KEY_CHECKING=False
-cd sowlastic
 mkdir ./group_vars
 read -p "Enter API key: " APIKEY
 echo "digio_api_key: $APIKEY" >> ./group_vars/all
