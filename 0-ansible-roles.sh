8f1b91fe62e355ad54249f0a9819beb0d888519d
Diox <gt@ks.diox.es>
2020-11-06 10:18:58 +0000

    Adding etc file for easier setup

--- 0-ansible-roles.sh
+++ 0-ansible-roles.sh
@@ -1,9 +1,14 @@
+#git clone https://github.com/gtr0y/sowlastic.git
 apt-get update
 apt-get install python3-pip -y
+apt-get install python3-jmespath -y
 pip3 install ansible
 ssh-keygen -q -t rsa -b 2048 -N '' -f ~/.ssh/id_rsa <<<n 2>&1 >/dev/null
 ansible-galaxy collection install community.general
 ansible-galaxy collection install community.digitalocean
 ansible-galaxy collection install community.crypto
 ansible-galaxy install elastic.elasticsearch,7.9.2
-git clone https://github.com/gtr0y/sowlastic.git
+mkdir ./group_vars
+echo "clustername: sowel-prod" >> ./group_vars/all
+read -p "Enter API key: " APIKEY
+echo "digio_api_key: $APIKEY" >> ./group_vars/all
